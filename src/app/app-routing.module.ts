19e9b3e42146088b9711e27b665dde0199f4034a
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-07 15:31:13 +0200

    update app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('../app/modules/guest/guest.module').then((m) => m.GuestModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('../app/modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'student',
+    loadChildren: () =>
+      import('../app/modules/student/student.module').then(
+        (m) => m.StudentModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
