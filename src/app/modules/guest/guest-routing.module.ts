c6305d64056b9a02ad5188e995966479f1c50e8b
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-07 15:19:34 +0200

    create main components in guest module

--- src/app/modules/guest/guest-routing.module.ts
+++ src/app/modules/guest/guest-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class GuestRoutingModule { }
+export class GuestRoutingModule {}
--- src/app/modules/guest/guest.module.ts
+++ src/app/modules/guest/guest.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GuestRoutingModule } from './guest-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     GuestRoutingModule
