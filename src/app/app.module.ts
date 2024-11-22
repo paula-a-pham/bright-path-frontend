01285c93b0ed6e42f7e17f56eba19cfc0d163602
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-22 22:30:44 +0200

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { CoreModule } from './core/core.module';
+import { firebaseConfig } from './core/constants/firebase-config';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, SharedModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    SharedModule,
+    CoreModule,
+  ],
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
