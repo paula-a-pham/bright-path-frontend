10a5496109b2c6fbc66e3c2c99e3b5a829be9a5d
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-23 20:34:39 +0200

    setup form in sign up component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,16 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-
+import { SharedModule } from '../../shared/shared.module';
+import { CoreModule } from '../../core/core.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    SignUpComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, SignUpComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, CoreModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const declarations = [NotFoundComponent];
-const imports = [CommonModule, HttpClientModule];
+const imports = [
+  CommonModule,
+  HttpClientModule,
+  FormsModule,
+  ReactiveFormsModule,
+];
 
 @NgModule({
   declarations: declarations,
