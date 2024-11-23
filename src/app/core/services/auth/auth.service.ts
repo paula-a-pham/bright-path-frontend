16fbd89709c1952bdc95aa04b43c57f171d461ad
Paula Ashraf <paula.a.pham@gmail.com>
2024-11-23 20:23:42 +0200

    create auth service

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private auth: Auth) {}
+}
---
+++ src/app/shared/models/iuser.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  fristName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+  role?: string;
+}
